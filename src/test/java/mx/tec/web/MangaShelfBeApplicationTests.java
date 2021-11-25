d58140471c10fd29399ad65acc522620c9beddad
cemarv25 <cesar.martinez0010@outlook.com>
2021-11-24 18:38:33 -0700

    initial commit

---
+++ src/test/java/mx/tec/web/MangaShelfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tec.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaShelfBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

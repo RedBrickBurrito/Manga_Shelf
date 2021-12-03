79ef3c30e7003ffad7223c37f1facc63959627aa
Scyruz <a01561869@itesm.mx>
2021-12-02 18:20:37 -0700

    To read list conecton to BE

--- src/containers/MangasList/index.tsx
+++ src/containers/MangasList/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import NavigationBar from "../../components/NavBar";
 import ToReadList from "../../components/ToReadList";
+import LoginService from "../../services/LoginService";
 import Manga from "../../types/Manga";
 import ToRead from "../../types/ToRead";
 
 interface ListState {
     toReadList: ToRead[];
+    newToRead: ToRead;
+    openAdded: boolean;
+    openDeleted: boolean;
+    title: string;
   }
 
 /**
@@ -15,6 +21,10 @@ class MangasList extends React.Component<{}, ListState> {
 
     state = {
         toReadList: [] as ToRead[],
+        newToRead: {} as ToRead,
+        openAdded: false,
+        openDeleted: false,
+        title: "Your mangas to read later",
       };
 
     /**
@@ -23,102 +33,116 @@ class MangasList extends React.Component<{}, ListState> {
      */
     render() {
     
-        return (        
-                <ToReadList 
-                toReadList={this.state.toReadList} 
-                deleteMangaFromToRead={this.deleteMangaFromToRead} 
-                addMangaToRead={this.addMangaToRead}
+        return (   
+            <>
+            <NavigationBar />
+            <ToReadList
+                    toReadList={this.state.toReadList}
+                    deleteMangaFromToRead={this.deleteMangaFromToRead}
+                    addMangaToRead={this.addMangaToRead}
+                    closeAlert={this.closeAlert} 
+                    newToRead={this.state.newToRead} 
+                    openAdded={this.state.openAdded} 
+                    openDeleted={this.state.openDeleted} 
+                    title={this.state.title}
                 />
+            </>
             );
     }
 
     componentDidMount(){
-        const toReadList = [
-            {
-                id: 1, 
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 2,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 3,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 4,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 5,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 6,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 7,
-                userId:3,
-                mangaId:333,   
-            },
-            {
-                id: 8,
-                userId:3,
-                mangaId:333, 
-            },
-        ];
 
-        const toReadListUpdate = [] as ToRead[];
+        LoginService.getToReadList(21)
+          .then(async(response) => {
+            const toReadList = response.data as ToRead[];
 
-        for (const toRead of toReadList) {
+            if (toReadList.length === 0){
+                const title = "You have no mangas to read"
+                this.setState({title});
+            } 
+            else{
 
-            const manga = {
-                id: 333,    
-                title: "Manga To Read Title",    
-                author: "Manga To Read Author", 
-                publicationDate: "01-03-2015"   
-            } as Manga;
+                const toReadListUpdate = [] as ToRead[];
 
-            const toReadUpdate = {...toRead, 
-                mangaTitle: manga.title,
-                mangaAuthor: manga.author,    
-                mangaPublicationDate: manga.publicationDate,
-            };
+                for (const toRead of toReadList) {
+        
+                    LoginService.getManga(toRead.mangaId)
+                    .then(async(response) => {
+                        const manga = response.data as Manga;
+    
+                        const toReadUpdate = {...toRead, 
+                            mangaTitle: manga.title as string,
+                            mangaAuthor: manga.author as string,    
+                        };
+            
+                        toReadListUpdate.push(toReadUpdate);    
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });                             
+                  }
+        
+                this.setState({ toReadList: toReadListUpdate});
+                console.log(this.state.toReadList);
+    
+                
+            }
 
-            toReadListUpdate.push(toReadUpdate);                       
-          }
-
-        this.setState({ toReadList: toReadListUpdate});
-
+            })
+          .catch((error) => {
+            console.log(error);
+            });          
     }
 
-    addMangaToRead(mangaId: any){
-        try
-        {
-            console.log("Manga "+ mangaId + " added to list");
+    addMangaToRead = (toRead: ToRead) => {
 
-        }
-        catch(e){
-            console.log("Error adding manga "+ mangaId + " to list");
-        }
+        LoginService.addToRead(toRead)
+            .then(async(response) => {
+                console.log(response);
+                console.log("Manga " + toRead.mangaId + " added to list");
+                
+                const newToRead = response.data as ToRead;
+
+                this.setState({newToRead, openAdded: true, openDeleted: false});
+            })
+            .catch((error) => {
+            console.log(error);
+            console.log("Error adding manga "+ toRead.mangaId + " to list");
+            });  
+        
     }
     
-    deleteMangaFromToRead(mangaId: any){
-        try
-        {
-            console.log("Manga "+ mangaId + " deleted from list");
+    deleteMangaFromToRead = (idToDelete: any) => {
+
+        LoginService.getToRead(idToDelete)
+        .then(async(response) => {
+            const newToRead = response.data as ToRead;
+            this.setState({newToRead});
+        })
+        .catch((error) => {
+        console.log(error);
+        });         
+        LoginService.deleteFromToReadList(idToDelete)
+        .then(async(response) => {
+            console.log(response)
+            console.log("Manga "+ idToDelete + " deleted from list");
+
+            this.setState({openAdded: false, openDeleted:true});
+        })
+        .catch((error) => {
+        console.log(error);
+        console.log("Error deleting manga "+ idToDelete + " from list");
+        }); 
+        
+    }
+
+    closeAlert = (alert: string) => {
+        if (alert === "added"){
+            this.setState({openAdded: false});
         }
-        catch(e)
-        {
-            console.log("Error deleting manga "+ mangaId + " from list");
+
+        if(alert === "deleted"){
+            this.setState({openDeleted:false});
         }
     }
 }
